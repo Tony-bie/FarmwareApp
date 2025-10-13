bae8b0c3368b3035ec4567a2ad305786c8dd8720
Santiago Cordova <A01029211@tec.mx>
2025-10-13 12:59:02 -0600

    Modelo de fotos implementado en la pagina de resultados

---
+++ taller/<nose/<nose/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  <nose
+//
+//  Created by Alumno on 18/09/25.
+//
+
+import Foundation
+
+
+struct Person {
+    var name: String
+    var direaccion : String
+    
+}
---
+++ taller/<nose/<nose/_noseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _noseApp.swift
+//  <nose
+//
+//  Created by Alumno on 18/09/25.
+//
+
+import SwiftUI
+
+@main
+struct _noseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
